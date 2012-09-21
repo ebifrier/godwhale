0717854eb1c0bbf04b00271d3f4a6d621be0d9c7
ebifrier <devnull@localhost>
2012-09-21 20:10:02 +0900

    後手番に対応しました。

--- server/run-server.sh
+++ server/run-server.sh
@@ -1,15 +1,14 @@
 perl majority_server.pl --client_port=4082 \
-                        --client_num=2 \
+                        --client_num=1 \
                         --csa_host=localhost \
                         --csa_port=4081 \
                         --csa_id=bonachan \
-                        --csa_pw=test-99999-99999-W \
+                        --csa_pw=test-99999-99999-B \
                         --sec_limit=0 \
                         --sec_limit_up=10 \
                         --time_response=0.15 \
                         --time_stable_min=11.0 \
                         --sec_spent_b=0 \
                         --sec_spent_w=0 \
-                        --noaudio
-
-#                        --final_as_confident
+                        --noaudio \
+                        --final_as_confident
