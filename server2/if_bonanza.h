9c5e4b9fa137f1f7f8b1811b4de05df8ded3c970
ebifrier <devnull@localhost>
2013-10-12 21:24:35 +0900

    元のbonanzaにインターフェースを提供するためのヘッダファイルを追加しました。

---
+++ server2/if_bonanza.h
@@ -0,0 +1,38 @@
+/* $Id: if_bonanza.h,v 1.4 2012/03/26 06:21:19 eikii Exp $ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "bonanza6/shogi.h"
+
+extern int DBG_MASTER, VMMODE;
+extern int slave_proc_offset;
+extern int master_proc_offset;
+extern FILE* slavelogfp;
+extern FILE* masterlogfp;
+
+extern int Mproc, Nproc, Ncomm;
+extern int x_dmy_for_calcinc, INCS_PER_USEC;
+
+ // for main()
+void mpi_init(int argc, char **argv, int *nproc, int *mproc);
+void mpi_close();
+void quitHook();
+void slave();
+
+ // for search()
+int detectSignalSlave();
+
+ // for thread
+void attachCpu(int cpu);
+
+ // for bonanza6
+void iniGameHook(const min_posi_t*);
+void makeMoveRootHook(int move);
+void unmakeMoveRootHook();
+int  master(unsigned int*);
+
+#ifdef __cplusplus
+}
+#endif
